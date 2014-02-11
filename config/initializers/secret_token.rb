df6b6a2e110038dc1ecb67dc9c012948c40edc1b
Eric <ericjiwonchoi@gmail.com>
2014-02-10 19:50:02 -0800

    bare app for class management

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ClassMgmt::Application.config.secret_key_base = '82965d3fa7fbb3f38e631acaf8faa594d3949cedfc48320e64c5e1a86098cabcbf8df94524c90981b29093d013d88c36f4702182192c8a71ba2de31f15d231ac'
